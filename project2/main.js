1d3d8bacfb86449b5be50e48af6d5e6f23dee480
OliviaXiao901 <wenxuxia@usc.edu>
2019-11-07 19:17:12 -0800

    update

--- project2/main.js
+++ project2/main.js
@@ -2,8 +2,11 @@ $(document).ready(function(){
 
   $("#story1").hide();
   $("#story2").hide();
+  $("#story3").hide();
   $(".question").hide();
   $(".button").hide();
+  $("#narrative2").hide();
+
 
   $(".startButton").click(function(){
     $(".startButton").hide();
@@ -30,9 +33,33 @@ $(document).ready(function(){
   $("#choice2").click(function(){
     $("#story1").hide();
     $("#card2").hide();
+    $("#choice2").hide();
     $("#story2").show(1000);
+  });
+
+  $("#narrative1").click(function(){
+    $(".narrative").hide();
     $("#choice3").show(1000);
     $("#choice4").show(1000);
+  });
+
+  $("#choice3").click(function(){
+    $("#panel2").slideToggle("slow");
+  });
+
+  $("#choice4").click(function(){
+    $("#choice3").hide();
+    $("#panel2").hide();
+    $("#choice4").hide();
+    $("#narrative2").show(1000);
+    $("#card2").show(1000);
+    $("#pic2").show(1000);
+  });
+
+  $("#pic2").click(function(){
+    $("#story2").hide();
+    $("#story3").show(1000);
+    $(".narrative").show(1000);
   });
 
 });
